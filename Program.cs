4e8513360c84ff378ca579b47ceebea569b7c289
Dekel Ari <dekelari@gmail.com>
2022-06-26 11:52:42 +0300

    scanning UART ports OK

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace sucrazit // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var serPort = new Serial_c();
+
+            serPort.serialPortFind(Const.port_detect_msg_send, Const.port_detect_msg_rcv);
+        }
+    }
+
+    public static class Const
+    {
+        public const string port_detect_msg_send = "";
+        public const string port_detect_msg_rcv = "";
+    }
+}
