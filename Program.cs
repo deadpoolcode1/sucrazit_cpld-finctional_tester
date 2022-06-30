9741bf485dbefd908cd4749d0a5170ba4c909506
Dekel Ari <Dekel-A@ZivavExt.local>
2022-06-30 18:35:43 +0300

    WIP

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ namespace sucrazit // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
