ddda30b128ee01fb4555f56a91518e013f5bfc3f
Dekel Ari <dekelari@gmail.com>
2022-07-03 14:47:05 +0300

    added sending full gpio set message

--- Program.cs
+++ Program.cs
@@ -4,18 +4,21 @@ namespace sucrazit // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            String port_name = "";
             Console.WriteLine("Hello World!");
-            var serPort = new Serial_c();
 
-            serPort.serialPortFind(Const.port_detect_msg_send, Const.port_detect_msg_rcv);
+            port_name = Serial_c.serialPortFind(Const.port_detect_msg_send, "");
             var m_Command = new Commands();
             m_Command.controller_set_gpio(GpioGroupConst.A, 1);
             m_Command.controller_get_gpio(GpioGroupConst.A, 1);
             var msgGpioIn = new Tuple<int, int>(GpioGroupConst.A, 1);
             var msgGpioOut = new Tuple<int, int>(GpioGroupConst.B, 2);
+            Serial_c.Connect(port_name);
             m_Command.send_test(msgGpioIn, msgGpioOut);
+            Serial_c.Disconnect();
         }
     }
 
